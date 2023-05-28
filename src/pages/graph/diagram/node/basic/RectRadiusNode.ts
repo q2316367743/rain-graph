d31f1badba838f33c4f5319861f97cc292085f4d
落雨不悔 <m17762618644@163.com>
2023-05-28 13:38:11 +0800

    带圆角的矩形修复

--- src/pages/graph/diagram/node/basic/RectRadiusNode.ts
+++ src/pages/graph/diagram/node/basic/RectRadiusNode.ts
@@ -1,10 +1,22 @@
 import { RectNode, RectNodeModel } from "@logicflow/core";
+import { getShapeStyleFunction, getTextStyleFunction } from "../getShapeStyleUtil";
 
 // 带圆角的矩形
 class RectRadiusModel extends RectNodeModel {
   setAttributes () {
     super.setAttributes()
     this.radius = 20;
+  }
+  getNodeStyle() {
+    const style = super.getNodeStyle()
+    const properties = this.getProperties()
+    return getShapeStyleFunction(style, properties)
+  }
+
+  getTextStyle() {
+    const style = super.getTextStyle()
+    const properties = this.getProperties()
+    return getTextStyleFunction(style, properties)
   }
 }
 export default {

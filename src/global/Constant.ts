4be855c23d1a4378262e5970a794664c6817c55a
落雨不悔 <m17762618644@163.com>
2023-06-01 22:49:20 +0800

    版本更新

--- src/global/Constant.ts
+++ src/global/Constant.ts
@@ -1,6 +1,6 @@
 export default {
     name: '听雨图编辑器',
-    version: '1.0.0',
+    version: '1.0.1',
     author: '落雨不悔',
     website: 'https://blog.esion.xyz',
     homepage: 'https://www.yuque.com/baozhiyige-tewwf/khg2sx/tbrfl0e57la92gm2',

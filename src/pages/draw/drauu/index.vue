c37570de84caadefa5762502f607cc3e260dbdfa
落雨不悔 <m17762618644@163.com>
2023-06-07 10:31:12 +0800

    多标签页

--- src/pages/draw/drauu/index.vue
+++ src/pages/draw/drauu/index.vue
@@ -1,229 +1,109 @@
 <template>
-    <div class="drauu w-screen h-screen flex flex-col select-none">
-        <!-- 顶部 -->
-        <div class="header">
-            <div class="color-group">
-                <div class="color-item" :class="color === '#f2f2f2' ? 'active' : ''" @click="color = '#f2f2f2'">​⚪</div>
-                <div class="color-item" :class="color === '#000000' ? 'active' : ''" @click="color = '#000000'">⚫️</div>
-                <div class="color-item" :class="color === '#ed153d' ? 'active' : ''" @click="color = '#ed153d'">​🔴</div>
-                <div class="color-item" :class="color === '#ed9a26' ? 'active' : ''" @click="color = '#ed9a26'">🟠</div>
-                <div class="color-item" :class="color === '#ede215' ? 'active' : ''" @click="color = '#ede215'">🟡</div>
-                <div class="color-item" :class="color === '#30bd20' ? 'active' : ''" @click="color = '#30bd20'">🟢</div>
-                <div class="color-item" :class="color === '#2656bf' ? 'active' : ''" @click="color = '#2656bf'">🔵​</div>
-                <div class="color-item" :class="color === '#c24aed' ? 'active' : ''" @click="color = '#c24aed'">🟣​</div>
-                <div class="color-item" :class="color === '#bf6b26' ? 'active' : ''" @click="color = '#bf6b26'">🟤</div>
-            </div>
-            <div style="text-align: center;">
-                <a-slider v-model="size" :min="1" :max="10" :step="0.5" style="width: 150px;margin-top: 10px;"
-                    show-tooltip />
-            </div>
-            <div style="text-align: right;">
-                <a-button-group type="text">
-                    <a-button title="后退" @click="undo" style="width: 32px;">↩️</a-button>
-                    <a-button title="前进" @click="redo" style="width: 32px;">↪️</a-button>
-                    <a-button title="清空" @click="clear" style="width: 32px;">🗑</a-button>
-                    <a-dropdown trigger="click">
-                        <a-button title="导出" style="width: 32px;">📥</a-button>
-                        <template #content>
-                            <a-doption @click="downloadToSvg">SVG</a-doption>
-                            <a-doption @click="downloadToPng">PNG</a-doption>
-                        </template>
-                    </a-dropdown>
-                </a-button-group>
-            </div>
+    <div class="drauu">
+        <div class="container" :style="{ bottom: show ? '40px' : '0px' }">
+            <drauu-instance v-for="instance in instances" :key="instance.id" :id="instance.id" v-show="instance.id === id" />
         </div>
-        <!-- 左侧边栏 -->
-        <div class="side">
-            <div class="side-btn" :class="brush === 'stylus' ? 'active' : ''" @click="brush = 'stylus'">✍️</div>
-            <div class="side-btn" :class="brush === 'draw' ? 'active' : ''" @click="brush = 'draw'">✏️</div>
-            <div class="side-btn" :class="brush === 'line' ? 'active' : ''" @click="brush = 'line'">⁄</div>
-            <div class="side-btn" :class="brush === 'arrow' ? 'active' : ''" @click="brush = 'arrow'">↗</div>
-            <div class="side-btn" :class="brush === 'rectangle' ? 'active' : ''" @click="brush = 'rectangle'">⃞</div>
-            <div class="side-btn" :class="brush === 'ellipse' ? 'active' : ''" style="font-size: 26px;"
-                @click="brush = 'ellipse'">○</div>
-            <div class="side-btn" :class="brush === 'eraseLine' ? 'active' : ''" @click="brush = 'eraseLine'">🧹</div>
-            <a-divider />
-            <div class="side-btn" :class="dasharray === 'solid' ? 'active' : ''" @click="dasharray = 'solid'">—</div>
-            <div class="side-btn" :class="dasharray === 'dashed' ? 'active' : ''" @click="dasharray = 'dashed'">┅</div>
-            <div class="side-btn" :class="dasharray === 'dotted' ? 'active' : ''" @click="dasharray = 'dotted'">⋯</div>
+        <div class="tabs" :style="{ height: show ? '39px' : '0px' }">
+            <a-tabs v-model:active-key="id" type="line" :editable="true" @add="handleAdd" @delete="handleDelete"
+                show-add-button auto-switch hide-content>
+                <a-tab-pane v-for="instance in instances" :key="instance.id" :title="instance.name" />
+            </a-tabs>
         </div>
-        <!-- 内容 -->
-        <div class="drauu-view-wrap">
-            <svg id="drauu-view" ref="drauu-view" style="touch-action: none"></svg>
+        <div class="toggle" :style="{ bottom: show ? '44px' : '4px' }" @click="show = !show">
+            <a-button type="text">
+                <template #icon><icon-tags /></template>
+            </a-button>
         </div>
-        <!-- 帮助信息 -->
-        <pre class="about">
-<b>Drauu</b>
-Headless SVG-based drawboard in browser
-https://github.com/antfu/drauu
-</pre>
     </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { createDrauu, Drauu, DrawingMode } from 'drauu';
-import { mapState } from "pinia";
-import { useGlobalStore } from "@/store/GlobalStore";
-import BrowserUtil from "@/utils/BrowserUtil";
-import MessageUtil from "@/utils/MessageUtil";
 
-let drauu: Drauu;
+import DrauuInstance from './instance.vue';
+
+interface Instance {
+
+    id: number;
+
+    name: string
+
+}
 
 export default defineComponent({
     name: 'drauu',
+    components: { DrauuInstance },
     data: () => ({
-        color: '#000000',
-        size: 3,
-        brush: 'stylus',
-        dasharray: 'solid'
+        instances: new Array<Instance>(),
+        id: 0,
+        show: false
     }),
-    computed: {
-        ...mapState(useGlobalStore, ['isDark'])
-    },
-    watch: {
-        color() {
-            if (drauu) {
-                drauu.brush.color = this.color;
-            }
-        },
-        size() {
-            drauu.brush.size = this.size;
-        },
-        brush() {
-            drauu.brush.arrowEnd = this.brush === 'arrow';
-            drauu.mode = this.brush === 'arrow' ? 'line' : this.brush as DrawingMode;
-        },
-        dasharray() {
-            if (this.dasharray === 'solid') {
-                drauu.brush.dasharray = undefined
-            } else if (this.dasharray === 'dashed') {
-                drauu.brush.dasharray = '4'
-            } else if (this.dasharray === 'dotted') {
-                drauu.brush.dasharray = '1 7'
-            }
-        }
-    },
     created() {
-        this.color = this.isDark ? '#f2f2f2' : '#000000';
-    },
-    mounted() {
-        drauu = createDrauu({
-            el: '#drauu-view',
-            brush: {
-                color: this.color,
-                size: this.size,
-            },
-        });
+        this.compensate();
     },
     methods: {
-        undo() { drauu.undo() },
-        redo() { drauu.redo() },
-        clear() { drauu.clear() },
-        downloadToSvg() {
-            drauu.el!.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
-            const data = drauu.el!.outerHTML || '';
-            const blob = new Blob([data], { type: 'image/svg+xml' })
-            const elem = window.document.createElement('a')
-            elem.href = window.URL.createObjectURL(blob)
-            elem.download = 'drauu.svg'
-            document.body.appendChild(elem)
-            elem.click()
-            document.body.removeChild(elem)
+        compensate() {
+            if (this.instances.length === 0) {
+                let instance = {
+                    id: new Date().getTime(),
+                    name: '画板'
+                };
+                this.instances.push(instance);
+                this.id = instance.id;
+            } else {
+                let index = this.instances.findIndex(i => i.id === this.id);
+                if (index === -1) {
+                    this.id = this.instances[0].id;
+                }
+            }
         },
-        downloadToPng() {
-            BrowserUtil.svg2png(this.$refs['drauu-view'] as SVGSVGElement)
-                .then(e => BrowserUtil.downloadByBase64(e))
-                .catch(e => MessageUtil.error("下载图片失败", e));
+        handleAdd() {
+            this.instances.push({
+                id: new Date().getTime(),
+                name: '画板'
+            });
+        },
+        handleDelete(id: any) {
+            let index = this.instances.findIndex(i => i.id === id);
+            if (index > -1) {
+                this.instances.splice(index, 1);
+            }
+            this.compensate();
         }
     }
 });
 </script>
 <style lang="less">
+@import url(./instance.less);
+
 .drauu {
     position: relative;
-    width: 100%;
     height: 100%;
+    width: 100%;
     overflow: hidden;
 
-    .header {
+    .container {
         position: absolute;
         top: 0;
         left: 0;
         right: 0;
-        padding: 3px;
-        border-bottom: 1px solid var(--color-neutral-3);
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-
-        .color-group {
-            display: flex;
-
-            .color-item {
-                height: 32px;
-                width: 32px;
-                cursor: pointer;
-                text-align: center;
-                line-height: 32px;
-                border-radius: 2px;
-
-                &:hover {
-                    background-color: var(--color-neutral-3);
-                }
-
-                &.active {
-                    background-color: var(--color-neutral-4);
-                }
-            }
-        }
-
+        bottom: 40px;
+        transition: 0.2s;
     }
 
-    .side {
+    .tabs {
         position: absolute;
-        top: 39px;
         left: 0;
-        bottom: 0;
-        padding: 6px 3px;
-        border-right: 1px solid var(--color-neutral-3);
-
-        .side-btn {
-            width: 32px;
-            height: 32px;
-            text-align: center;
-            line-height: 32px;
-            cursor: pointer;
-            border-radius: 2px;
-
-            &:hover {
-                background-color: var(--color-neutral-3);
-            }
-
-            &.active {
-                background-color: var(--color-neutral-4);
-            }
-        }
-    }
-
-    .drauu-view-wrap {
-        position: absolute;
-        top: 39px;
-        left: 39px;
         right: 0;
         bottom: 0;
+        height: 39px;
+        border-top: 1px solid var(--color-neutral-3);
+        transition: 0.2s;
     }
 
-    .about {
+    .toggle {
         position: absolute;
-        right: 2rem;
-        bottom: 1rem;
-        pointer-events: none;
-        opacity: 0.35;
+        left: 4px;
+        transition: 0.2s;
     }
-
-}
-
-#drauu-view {
-    width: 100%;
-    height: 100%;
 }
 </style>
---
+++ src/pages/draw/drauu/instance.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="drauu-instance">
+        <!-- 顶部 -->
+        <div class="header">
+            <div class="color-group">
+                <div class="color-item" :class="color === '#f2f2f2' ? 'active' : ''" @click="color = '#f2f2f2'">​⚪</div>
+                <div class="color-item" :class="color === '#000000' ? 'active' : ''" @click="color = '#000000'">⚫️</div>
+                <div class="color-item" :class="color === '#ed153d' ? 'active' : ''" @click="color = '#ed153d'">​🔴</div>
+                <div class="color-item" :class="color === '#ed9a26' ? 'active' : ''" @click="color = '#ed9a26'">🟠</div>
+                <div class="color-item" :class="color === '#ede215' ? 'active' : ''" @click="color = '#ede215'">🟡</div>
+                <div class="color-item" :class="color === '#30bd20' ? 'active' : ''" @click="color = '#30bd20'">🟢</div>
+                <div class="color-item" :class="color === '#2656bf' ? 'active' : ''" @click="color = '#2656bf'">🔵​</div>
+                <div class="color-item" :class="color === '#c24aed' ? 'active' : ''" @click="color = '#c24aed'">🟣​</div>
+                <div class="color-item" :class="color === '#bf6b26' ? 'active' : ''" @click="color = '#bf6b26'">🟤</div>
+            </div>
+            <div style="text-align: center;">
+                <a-slider v-model="size" :min="1" :max="10" :step="0.5" style="width: 150px;margin-top: 10px;"
+                    show-tooltip />
+            </div>
+            <div style="text-align: right;">
+                <a-button-group type="text">
+                    <a-button title="后退" @click="undo" style="width: 32px;">↩️</a-button>
+                    <a-button title="前进" @click="redo" style="width: 32px;">↪️</a-button>
+                    <a-button title="清空" @click="clear" style="width: 32px;">🗑</a-button>
+                    <a-dropdown trigger="click">
+                        <a-button title="导出" style="width: 32px;">📥</a-button>
+                        <template #content>
+                            <a-doption @click="downloadToSvg">SVG</a-doption>
+                            <a-doption @click="downloadToPng">PNG</a-doption>
+                        </template>
+                    </a-dropdown>
+                </a-button-group>
+            </div>
+        </div>
+        <!-- 左侧边栏 -->
+        <div class="side">
+            <div class="side-btn" :class="brush === 'stylus' ? 'active' : ''" @click="brush = 'stylus'">✍️</div>
+            <div class="side-btn" :class="brush === 'draw' ? 'active' : ''" @click="brush = 'draw'">✏️</div>
+            <div class="side-btn" :class="brush === 'line' ? 'active' : ''" @click="brush = 'line'">⁄</div>
+            <div class="side-btn" :class="brush === 'arrow' ? 'active' : ''" @click="brush = 'arrow'">↗</div>
+            <div class="side-btn" :class="brush === 'rectangle' ? 'active' : ''" @click="brush = 'rectangle'">⃞</div>
+            <div class="side-btn" :class="brush === 'ellipse' ? 'active' : ''" style="font-size: 26px;"
+                @click="brush = 'ellipse'">○</div>
+            <div class="side-btn" :class="brush === 'eraseLine' ? 'active' : ''" @click="brush = 'eraseLine'">🧹</div>
+            <a-divider />
+            <div class="side-btn" :class="dasharray === 'solid' ? 'active' : ''" @click="dasharray = 'solid'">—</div>
+            <div class="side-btn" :class="dasharray === 'dashed' ? 'active' : ''" @click="dasharray = 'dashed'">┅</div>
+            <div class="side-btn" :class="dasharray === 'dotted' ? 'active' : ''" @click="dasharray = 'dotted'">⋯</div>
+        </div>
+        <!-- 内容 -->
+        <div class="drauu-view-wrap">
+            <svg class="drauu-view" :id="`drauu-view-${id}`" style="touch-action: none"></svg>
+        </div>
+        <!-- 帮助信息 -->
+        <pre class="about">
+<b>Drauu</b>
+Headless SVG-based drawboard in browser
+https://github.com/antfu/drauu
+</pre>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { createDrauu, Drauu, DrawingMode } from 'drauu';
+import { mapState } from "pinia";
+import { useGlobalStore } from "@/store/GlobalStore";
+import BrowserUtil from "@/utils/BrowserUtil";
+import MessageUtil from "@/utils/MessageUtil";
+import { markRaw } from "vue";
+
+
+export default defineComponent({
+    name: 'drauu-instance',
+    props: {
+        id: {
+            type: Number,
+            required: false,
+            default: new Date().getTime()
+        }
+    },
+    data: () => ({
+        color: '#000000',
+        size: 3,
+        brush: 'stylus',
+        dasharray: 'solid',
+        drauu: markRaw({} as Drauu)
+    }),
+    computed: {
+        ...mapState(useGlobalStore, ['isDark'])
+    },
+    watch: {
+        color() {
+            if (this.drauu) {
+                this.drauu.brush.color = this.color;
+            }
+        },
+        size() {
+            this.drauu.brush.size = this.size;
+        },
+        brush() {
+            this.drauu.brush.arrowEnd = this.brush === 'arrow';
+            this.drauu.mode = this.brush === 'arrow' ? 'line' : this.brush as DrawingMode;
+        },
+        dasharray() {
+            if (this.dasharray === 'solid') {
+                this.drauu.brush.dasharray = undefined
+            } else if (this.dasharray === 'dashed') {
+                this.drauu.brush.dasharray = '4'
+            } else if (this.dasharray === 'dotted') {
+                this.drauu.brush.dasharray = '1 7'
+            }
+        }
+    },
+    created() {
+        this.color = this.isDark ? '#f2f2f2' : '#000000';
+    },
+    mounted() {
+        this.drauu = markRaw(createDrauu({
+            el: `#drauu-view-${this.id}`,
+            brush: {
+                color: this.color,
+                size: this.size,
+            },
+        }));
+    },
+    methods: {
+        undo() { this.drauu.undo() },
+        redo() { this.drauu.redo() },
+        clear() { this.drauu.clear() },
+        downloadToSvg() {
+            this.drauu.el!.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+            const data = this.drauu.el!.outerHTML || '';
+            const blob = new Blob([data], { type: 'image/svg+xml' })
+            const elem = window.document.createElement('a')
+            elem.href = window.URL.createObjectURL(blob)
+            elem.download = 'drauu.svg'
+            document.body.appendChild(elem)
+            elem.click()
+            document.body.removeChild(elem)
+        },
+        downloadToPng() {
+            BrowserUtil.svg2png(this.drauu.el!)
+                .then(e => BrowserUtil.downloadByBase64(e))
+                .catch(e => MessageUtil.error("下载图片失败", e));
+        }
+    }
+});
+</script>
+<style lang="less"></style>

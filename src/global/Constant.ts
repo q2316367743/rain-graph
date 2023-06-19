d8b1483d282bdac8c46e59a1c9ea9306090b7690
落雨不悔 <m17762618644@163.com>
2023-06-19 18:48:05 +0800

    版本更新

--- src/global/Constant.ts
+++ src/global/Constant.ts
@@ -2,7 +2,7 @@ export default {
     uid: 'znzdzrrx',
     id: 'rain-graph',
     name: '听雨图编辑器',
-    version: '1.0.7',
+    version: '1.0.8',
     author: '落雨不悔',
     website: 'https://blog.esion.xyz',
     homepage: 'https://www.yuque.com/baozhiyige-tewwf/khg2sx/tbrfl0e57la92gm2',
